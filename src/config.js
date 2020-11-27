f7cea72aaba6063a8b9fce705edc3cc9cb34fdd5
Dan Holmes <dh@danielholmes.com>
2020-11-27 17:56:14 +0000

    Reset base url.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-//const BASE_URL = 'https://floating-falls-13250.herokuapp.com';
-const BASE_URL = 'http://localhost:1234'
+const BASE_URL = 'https://floating-falls-13250.herokuapp.com';
 
 export default BASE_URL;
